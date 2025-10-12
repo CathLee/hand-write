ae276eb2428c33972f41ac984e3e1d697be4d1aa
CathLee <447932704@qq.com>
2025-10-12 17:00:27 +0800

    chore:server

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,17 @@
+/*
+ * @Date: 2025-10-10 21:39:32
+ * @Description: 
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:3001/api',
+    }
+  }
 })
